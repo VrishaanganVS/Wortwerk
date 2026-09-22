60cefc0b3c373df0f8907fa5a12696c480465e4b
V2-hub <teknobot24@gmail.com>
2026-09-22 18:49:31 +0530

    Making the statcard which tells us the words learned with its description and the total no.of words learned

--- app/components/StatCard.tsx
+++ app/components/StatCard.tsx
@@ -1,8 +1,27 @@
 type StatCardProps = {
-title: string
-value: string
-description: string
-
+  title: string
+  value: string
+  description: string
 }
 
+export default function StatCard({
+  title,
+  value,
+  description,
+}: StatCardProps) {
+  return (
+    <div className="rounded-2xl border border-[#dedfd5] bg-white p-5">
+      <p className="text-sm text-[#68786f]">
+        {title}
+      </p>
 
+      <h2 className="mt-2 text-3xl font-bold">
+        {value}
+      </h2>
+
+      <p className="mt-1 text-sm text-[#68786f]">
+        {description}
+      </p>
+    </div>
+  )
+}
